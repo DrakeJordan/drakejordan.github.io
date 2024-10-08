43e2a9014da315fe007a2d75ff3f17ff0f6e202b
Drake Jordan <hi@drakejordan.com>
2024-10-08 13:31:55 -0600

    Added terrain gen demo

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -1,15 +1,39 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Terrain Generation Demo
 
+let terrain = [];
+const NUMBER_OF_RECTS = 20000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let howWide = width/NUMBER_OF_RECTS
+  generateTerrain(howWide);
 }
 
 function draw() {
   background(220);
+  for(let someRect of terrain) {
+    rect(someRect.x, someRect.y, someRect.w, someRect.h);
+  }
+}
+
+function generateTerrain(theWidth) {
+  let time = 0;
+  let deltaTime = 0.0003;
+  for (let x = 0; x < width; x += theWidth) {
+    let theHeight = noise(time) * height;
+    let someRect = spawnRectangle(x, theHeight, theWidth);
+    terrain.push(someRect);
+    time += deltaTime;
+  }
+}
+
+function spawnRectangle(leftSide, rectHeight, rectWidth) {
+  let theRect = {
+    x: leftSide,
+    y: height - rectHeight,
+    w: rectWidth,
+    h: rectHeight,
+  };
+
+  return theRect;
 }
